ea99b8201128ad47bfc4c36765d19a232cae4f35
adamhuazini <adamhuzaini1@gmail.com>
2023-04-04 19:10:33 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace taks_taxi
+{
+    class program
+    {
+        static void main(string[] args)
+        {
+            taxi taxi = new taxi();
+
+            taxi.drivername = "jamal";
+            taxi.onduty = true;
+            taxi.numpassenger = 10;
+
+            taxi.taxiinfo();
+            taxi.pickupassenger();
+            taxi.droppassenger();
+
+            Console.ReadLine();
+
+        }
+
+
+    }
+}
---
+++ taxi.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace taks_taxi
+{
+    internal class taxi
+    {
+        public string drivername { get; set; }
+        public bool onduty { get; set; }
+        public int numpassenger { get; set; }
+
+        public void taxiinfo()
+        {
+            Console.WriteLine("Driver Name : {0}", drivername);
+            if (onduty)
+            {
+                Console.WriteLine("On Duty : Yes ");
+            }
+            else 
+            {
+                Console.WriteLine("On Duty : No");
+            }
+
+            Console.WriteLine
+                ("Number Passenger : {0}", numpassenger);
+        }
+        public void pickupassenger()
+        {
+            Console.WriteLine("{0} sedang menjemput penumpang", drivername);
+        }
+        public void droppassenger()
+        {
+            Console.WriteLine("{0} sedang mengantarkan penumpang" , drivername);
+        }
+    }
+}
